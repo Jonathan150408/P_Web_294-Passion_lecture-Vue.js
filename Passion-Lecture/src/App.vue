f0e1a8b802e0696d2cd8daaafdda8902d0da5b27
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-02-24 16:27:21 +0100

    feat(routeur) : ajouter les routes de base afin d'accèder aux pages
    [30][WIP]
    Actuellement il n'y a que les routes de base afin d'accèder aux pages de l'app

--- Passion-Lecture/src/App.vue
+++ Passion-Lecture/src/App.vue
@@ -6,7 +6,12 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     Header
     <nav>
-      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/">Accueil</RouterLink>
+      <RouterLink to="/books">Livres</RouterLink>
+      <!-- il faudra remplacer les valeurs 1 par un id interactif -->
+      <RouterLink to="/books/1">Un livre spécifique (!changer!)</RouterLink>
+      <RouterLink to="/users/1">Un utilisateur spécifique (!changer!)</RouterLink>
+      <RouterLink to="/books/category/1">Une catégorie spécifique (!changer!)</RouterLink>
     </nav>
   </header>
 
--- Passion-Lecture/src/router/index.js
+++ Passion-Lecture/src/router/index.js
@@ -6,9 +6,42 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'home?limit=5',
       component: Home,
     },
+    {
+      path: '/books',
+      name: 'livres',
+      //IA : import asynchrone
+      component: () => import('../views/Books/Main.vue'),
+    },
+    {
+      path: '/books/:book_id',
+      name: 'livre',
+      //IA : import asynchrone
+      component: () => import('../views/Books/Book.vue'),
+    },
+    {
+      path: '/users/:user_id/books',
+      name: "livres de l'utilisateur",
+      //IA : import asynchrone
+      component: () => import('../views/Books/MyBooks.vue'),
+    },
+    {
+      path: '/books/category/:category_id',
+      name: 'livres par catégorie',
+      //IA : import asynchrone
+      component: () => import('../views/Books/Categorie.vue'),
+    },
+    /*
+    il manque 
+    - Ajouter un livre
+    - ajouter un commentaire
+    - modifier un livre
+    - modifier un commentaire
+    - supprimer un livre
+    - supprimer un commentaire
+     */
   ],
 })
 
