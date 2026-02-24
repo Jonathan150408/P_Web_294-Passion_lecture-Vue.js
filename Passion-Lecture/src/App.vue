a183943940ddabb925f2424c8597e61fd2628701
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-02-24 17:20:21 +0100

    feat(liens sur toutes les pages) : ajouter les liens
    [7][Done]

--- Passion-Lecture/src/App.vue
+++ Passion-Lecture/src/App.vue
@@ -4,20 +4,61 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    Header
-    <nav>
-      <RouterLink to="/">Accueil</RouterLink>
-      <RouterLink to="/books">Livres</RouterLink>
-      <!-- il faudra remplacer les valeurs 1 par un id interactif -->
-      <RouterLink to="/books/1">Un livre spécifique (!changer!)</RouterLink>
-      <RouterLink to="/users/1">Un utilisateur spécifique (!changer!)</RouterLink>
-      <RouterLink to="/books/category/1">Une catégorie spécifique (!changer!)</RouterLink>
-    </nav>
+    <RouterLink to="/">
+      <img src="./assets/MM-P_Web_294-Logo-Cropped.png" alt="Logo du site" />
+    </RouterLink>
+    <img src="./assets/MM-logo_utilisateur.webp" alt="Logo d'un utilisateur" />
   </header>
 
-  <RouterView />
+  <main>
+    <aside>
+      <nav>
+        <RouterLink to="/">Accueil</RouterLink>
+        <RouterLink to="/books">Livres</RouterLink>
+        <!-- il faudra remplacer les valeurs 1 par un id interactif -->
+        <RouterLink to="/users/1">Un utilisateur spécifique (!changer!)</RouterLink>
+        <RouterLink to="/books/category/1">La liste des catégories</RouterLink>
+      </nav>
+    </aside>
+    <RouterView />
+  </main>
 
-  <footer>Footer</footer>
+  <footer>
+    Tous droits réservés © 2026
+    <ul>
+      <li>
+        <a href="mailto:neo.darbellay@eduvaud.ch">Néo</a>
+      </li>
+      <li>
+        <a href="mailto:jonathan.junod@eduvaud.ch">Jonathan</a>
+      </li>
+    </ul>
+  </footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  display: grid;
+  grid-template-columns: 1fr 4fr 1fr;
+}
+
+nav {
+  display: flex;
+  flex-direction: column;
+  gap: 0;
+}
+
+nav a {
+  display: block;
+  padding: 15px 20px;
+  text-decoration: none;
+  color: inherit;
+  border: 1px solid #ccc;
+  text-align: center;
+}
+
+nav a:hover {
+  color: #fff;
+  background-color: #000;
+}
+</style>
