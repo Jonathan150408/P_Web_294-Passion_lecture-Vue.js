409c8e43beaa7eb9f1f48ce0ca6975c0440d5524
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-02-24 16:59:18 +0100

    feat(page d'accueil) : ajout de la base html
    [10][Done]

--- Passion-Lecture/src/views/Home.vue
+++ Passion-Lecture/src/views/Home.vue
@@ -1,7 +1,27 @@
 <script setup></script>
 
 <template>
-  <main><p>Page d'accueil</p></main>
+  <main>
+    <!-- lister les catégories -->
+    <ul>
+      <li class="categorie">
+        <h2>Nom de la catégorie</h2>
+        <!-- lister les livres -->
+        <ul>
+          <li class="livre">
+            <!-- ! CHANGER - routerlink statique pour le moment -->
+            <RouterLink to="/books/1">
+              <img src="" alt="Couverture du livre" />
+              <p>Auteur</p>
+              <strong>Titre du livre</strong>
+            </RouterLink>
+          </li>
+        </ul>
+        <!-- lien vers la page de la catégorie -->
+        <RouterLink to="/books/category/1">La liste des catégories</RouterLink>
+      </li>
+    </ul>
+  </main>
 </template>
 
 <style scoped></style>
