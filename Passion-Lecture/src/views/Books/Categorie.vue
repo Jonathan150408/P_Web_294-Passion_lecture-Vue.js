aaf59ddc89539add2af2c4a4710174ba9dcb2e4c
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-02-25 08:57:37 +0100

    feat(page d'une catégorie) : ajout de la base html
    [10][Done]

--- Passion-Lecture/src/views/Books/Categorie.vue
+++ Passion-Lecture/src/views/Books/Categorie.vue
@@ -1,7 +1,46 @@
 <script setup></script>
 
 <template>
-  <main><p>Category</p></main>
+  <main>
+    <div>
+      <h1>Nom de la catégorie</h1>
+      <div class="recherche">
+        <p>Rechercher</p>
+        <img src="" alt="image de loupe" />
+      </div>
+    </div>
+    <!-- Lister les livres -->
+    <ul>
+      <li>
+        <div>
+          <p>Auteur du livre</p>
+          <h3>Titre du livre</h3>
+        </div>
+        <img src="" alt="Background : couverture du livre" />
+      </li>
+      <li>
+        <div>
+          <p>Auteur du livre</p>
+          <h3>Titre du livre</h3>
+        </div>
+        <img src="" alt="Background : couverture du livre" />
+      </li>
+      <li>
+        <div>
+          <p>Auteur du livre</p>
+          <h3>Titre du livre</h3>
+        </div>
+        <img src="" alt="Background : couverture du livre" />
+      </li>
+      <li>
+        <div>
+          <p>Auteur du livre</p>
+          <h3>Titre du livre</h3>
+        </div>
+        <img src="" alt="Background : couverture du livre" />
+      </li>
+    </ul>
+  </main>
 </template>
 
 <style scoped></style>
