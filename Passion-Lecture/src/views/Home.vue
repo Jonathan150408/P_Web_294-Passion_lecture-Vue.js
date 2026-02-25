594395947c03194207cd8a8cdcd88dd2a37c1863
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-02-25 08:35:45 +0100

    feat(vue Accueil) : ajout de la base html
    [19][Done]

--- Passion-Lecture/src/views/Home.vue
+++ Passion-Lecture/src/views/Home.vue
@@ -2,25 +2,75 @@
 
 <template>
   <main>
-    <!-- lister les catégories -->
-    <ul>
-      <li class="categorie">
-        <h2>Nom de la catégorie</h2>
-        <!-- lister les livres -->
-        <ul>
-          <li class="livre">
-            <!-- ! CHANGER - routerlink statique pour le moment -->
-            <RouterLink to="/books/1">
-              <img src="" alt="Couverture du livre" />
-              <p>Auteur</p>
-              <strong>Titre du livre</strong>
-            </RouterLink>
-          </li>
-        </ul>
-        <!-- lien vers la page de la catégorie -->
-        <RouterLink to="/books/category/1">La liste des catégories</RouterLink>
-      </li>
-    </ul>
+    <div>
+      <div><h1>Bienvenue</h1></div>
+      <img src="../assets/MM-P_Web_294-Logo-Cropped.png" alt="Bannière du site / hero" />
+    </div>
+    <div>
+      <h2>Notre But</h2>
+      <p>
+        Notre site Bibliothèque est un espace communautaire dédié aux passionnés de lecture. À la
+        fois base de données de livres et lieu d'échange, il permet de découvrir des ouvrages,
+        consulter des fiches détaillées et explorer une vaste collection enrichie par la communauté.
+        Les utilisateurs peuvent publier des avis, partager leurs impressions, noter les livres et
+        participer à des discussions, à la manière d'un Reddit consacré à la littérature. Notre
+        objectif : créer un espace vivant où chaque lecteur peut recommander, débattre et
+        construire, ensemble, une bibliothèque collaborative et interactive. 📚
+      </p>
+      <h2>Les 5 derniers ajouts</h2>
+      <ul>
+        <li>
+          <div>
+            <div>
+              <p>Éditeur</p>
+              <p>Type (BD)</p>
+            </div>
+            <p>Titre du livre</p>
+          </div>
+          <img src="" alt="En background : couverture du livre" />
+        </li>
+        <li>
+          <div>
+            <div>
+              <p>Éditeur</p>
+              <p>Type (BD)</p>
+            </div>
+            <p>Titre du livre</p>
+          </div>
+          <img src="" alt="En background : couverture du livre" />
+        </li>
+        <li>
+          <div>
+            <div>
+              <p>Éditeur</p>
+              <p>Type (BD)</p>
+            </div>
+            <p>Titre du livre</p>
+          </div>
+          <img src="" alt="En background : couverture du livre" />
+        </li>
+        <li>
+          <div>
+            <div>
+              <p>Éditeur</p>
+              <p>Type (BD)</p>
+            </div>
+            <p>Titre du livre</p>
+          </div>
+          <img src="" alt="En background : couverture du livre" />
+        </li>
+        <li>
+          <div>
+            <div>
+              <p>Éditeur</p>
+              <p>Type (BD)</p>
+            </div>
+            <p>Titre du livre</p>
+          </div>
+          <img src="" alt="En background : couverture du livre" />
+        </li>
+      </ul>
+    </div>
   </main>
 </template>
 
