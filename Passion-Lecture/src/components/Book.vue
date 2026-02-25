98a80c47554fde0aa336630cee3cfc3b5a34a9c9
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-02-25 09:32:28 +0100

    feat(composant book) : ajout de la base html
    [5][Done]

--- Passion-Lecture/src/components/Book.vue
+++ Passion-Lecture/src/components/Book.vue
@@ -13,11 +13,13 @@ defineProps({
 </script>
 
 <template>
-  <p>A remplir</p>
+  <RouterLink to="/books/1">
+    <div>
+      <p>Auteur du livre</p>
+      <h3>Titre du livre</h3>
+    </div>
+    <img src="" alt="Background : couverture du livre" />
+  </RouterLink>
 </template>
 
-<style scoped>
-p {
-  color: red;
-}
-</style>
+<style scoped></style>
