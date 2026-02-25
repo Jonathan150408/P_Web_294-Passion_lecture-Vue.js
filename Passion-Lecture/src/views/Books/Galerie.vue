f10dc62c568e866c2e2df45742bbbdacc156d06f
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-02-25 11:25:53 +0100

    feat(page galerie) : rendre la page interactive
    [5][WIP]

--- Passion-Lecture/src/views/Books/Galerie.vue
+++ Passion-Lecture/src/views/Books/Galerie.vue
@@ -1,46 +1,51 @@
 <script setup>
 import Book from '../../components/Book.vue'
+import HttpService from '@/services/HttpService'
+import { ref, onMounted } from 'vue'
+
+//import des livres
+const books = ref(null)
+onMounted(() => {
+  HttpService.getBooks()
+    .then((response) => {
+      books.value = response.data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
+
+//import des catégories
+const categories = ref(null)
+onMounted(() => {
+  HttpService.getCategories()
+    .then((response) => {
+      categories.value = response.data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
 </script>
 
 <template>
   <main>
-    <section>
-      <!-- catégorie 1 -->
+    <section v-for="(categorie, index) in categories" :key="index">
+      <!-- catégorie -->
       <div>
-        <h2>Titre de la catégorie</h2>
-        <RouterLink to="/books/category/1">Voir plus (redirige vers catégorie)</RouterLink>
+        <h2>{{ categorie.label }}</h2>
+        <RouterLink :to="`/books/category/${categorie.id}`"
+          >Voir plus (redirige vers catégorie)</RouterLink
+        >
       </div>
       <!-- liste des livres -->
       <div>
-        <Book></Book>
-        <Book></Book>
-        <Book></Book>
-      </div>
-    </section>
-    <section>
-      <!-- catégorie 2 -->
-      <div>
-        <h2>Titre de la catégorie</h2>
-        <RouterLink to="/books/category/1">Voir plus (redirige vers catégorie)</RouterLink>
-      </div>
-      <!-- liste des livres -->
-      <div>
-        <Book></Book>
-        <Book></Book>
-        <Book></Book>
-      </div>
-    </section>
-    <section>
-      <!-- catégorie 3 -->
-      <div>
-        <h2>Titre de la catégorie</h2>
-        <RouterLink to="/books/category/1">Voir plus (redirige vers catégorie)</RouterLink>
-      </div>
-      <!-- liste des livres -->
-      <div>
-        <Book></Book>
-        <Book></Book>
-        <Book></Book>
+        <Book
+          v-for="(book, index) in books"
+          :key="index"
+          :book="book"
+          v-show="book.categoryId === categorie.id"
+        ></Book>
       </div>
     </section>
   </main>
