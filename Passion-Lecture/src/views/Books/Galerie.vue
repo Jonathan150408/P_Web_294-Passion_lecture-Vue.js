5e86710bc6f085f18f59d34e746c4b0362c2250d
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-02-25 08:50:06 +0100

    feat(page Galerie) : ajout de la base html
    [20][Done]

--- Passion-Lecture/src/views/Books/Galerie.vue
+++ Passion-Lecture/src/views/Books/Galerie.vue
@@ -1,7 +1,122 @@
 <script setup></script>
 
 <template>
-  <main><p>Tous les livres</p></main>
+  <main>
+    <section>
+      <!-- catégorie 1 -->
+      <div>
+        <h2>Titre de la catégorie</h2>
+        <RouterLink to="/books/category/1">Voir plus (redirige vers catégorie)</RouterLink>
+      </div>
+      <!-- liste des livres -->
+      <ul>
+        <li>
+          <div>
+            <p>Auteur du livre</p>
+            <h3>Titre du livre</h3>
+          </div>
+          <img src="" alt="Background : couverture du livre" />
+        </li>
+        <li>
+          <div>
+            <p>Auteur du livre</p>
+            <h3>Titre du livre</h3>
+          </div>
+          <img src="" alt="Background : couverture du livre" />
+        </li>
+        <li>
+          <div>
+            <p>Auteur du livre</p>
+            <h3>Titre du livre</h3>
+          </div>
+          <img src="" alt="Background : couverture du livre" />
+        </li>
+        <li>
+          <div>
+            <p>Auteur du livre</p>
+            <h3>Titre du livre</h3>
+          </div>
+          <img src="" alt="Background : couverture du livre" />
+        </li>
+      </ul>
+    </section>
+    <section>
+      <!-- catégorie 2 -->
+      <div>
+        <h2>Titre de la catégorie</h2>
+        <RouterLink to="/books/category/1">Voir plus (redirige vers catégorie)</RouterLink>
+      </div>
+      <!-- liste des livres -->
+      <ul>
+        <li>
+          <div>
+            <p>Auteur du livre</p>
+            <h3>Titre du livre</h3>
+          </div>
+          <img src="" alt="Background : couverture du livre" />
+        </li>
+        <li>
+          <div>
+            <p>Auteur du livre</p>
+            <h3>Titre du livre</h3>
+          </div>
+          <img src="" alt="Background : couverture du livre" />
+        </li>
+        <li>
+          <div>
+            <p>Auteur du livre</p>
+            <h3>Titre du livre</h3>
+          </div>
+          <img src="" alt="Background : couverture du livre" />
+        </li>
+        <li>
+          <div>
+            <p>Auteur du livre</p>
+            <h3>Titre du livre</h3>
+          </div>
+          <img src="" alt="Background : couverture du livre" />
+        </li>
+      </ul>
+    </section>
+    <section>
+      <!-- catégorie 3 -->
+      <div>
+        <h2>Titre de la catégorie</h2>
+        <RouterLink to="/books/category/1">Voir plus (redirige vers catégorie)</RouterLink>
+      </div>
+      <!-- liste des livres -->
+      <ul>
+        <li>
+          <div>
+            <p>Auteur du livre</p>
+            <h3>Titre du livre</h3>
+          </div>
+          <img src="" alt="Background : couverture du livre" />
+        </li>
+        <li>
+          <div>
+            <p>Auteur du livre</p>
+            <h3>Titre du livre</h3>
+          </div>
+          <img src="" alt="Background : couverture du livre" />
+        </li>
+        <li>
+          <div>
+            <p>Auteur du livre</p>
+            <h3>Titre du livre</h3>
+          </div>
+          <img src="" alt="Background : couverture du livre" />
+        </li>
+        <li>
+          <div>
+            <p>Auteur du livre</p>
+            <h3>Titre du livre</h3>
+          </div>
+          <img src="" alt="Background : couverture du livre" />
+        </li>
+      </ul>
+    </section>
+  </main>
 </template>
 
 <style scoped></style>
