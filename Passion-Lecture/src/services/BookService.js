388f17e02eb9f0c4c93bfe23434367600015def1
neo-darbellay <178883466+neo-darbellay@users.noreply.github.com>
2026-03-04 08:13:28 +0100

    feat(BookService): Modifié l'ajout de livres et modification de livres
    [9][Done]

--- Passion-Lecture/src/services/BookService.js
+++ Passion-Lecture/src/services/BookService.js
@@ -22,9 +22,35 @@ export default {
     return apiClient.get('/' + id)
   },
   //post pour ajouter un livre
-  addBook(book) {
-    console.log('addbook called')
+  async addBook(book) {
+    console.log('addBook called')
     console.log(book)
+
+    const response = await apiClient.get('/?_sort=id&_order=desc&_limit=1')
+    const books = response.data
+
+    const newId = books.length > 0 ? Number(books[0].id) + 1 : 1
+
+    const now = new Date().toISOString()
+
+    book.id = String(newId)
+    book.createdAt = now
+    book.updatedAt = now
+
     return apiClient.post('/', book)
   },
+  // put pour modifier un livre
+  async updateBook(book) {
+    console.log('updateBook called')
+    console.log(book)
+
+    const now = new Date().toISOString()
+
+    const updatedBook = {
+      ...book,
+      updatedAt: now
+    }
+
+    return apiClient.put(`/${book.id}`, updatedBook)
+  }
 }
