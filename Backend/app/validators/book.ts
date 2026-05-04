bc6f05cd98d7df2c45c69d93dcb8c54186bb95f0
jonathan150408 <196329763+Jonathan150408@users.noreply.github.com>
2026-05-04 13:38:47 +0200

    feat(validator book) : terminer le validateur book
    [19][Done]

---
+++ Backend/app/validators/book.ts
@@ -0,0 +1,34 @@
+import Book from '#models/book'
+import User from '#models/user'
+import Writer from '#models/writer'
+import vine from '@vinejs/vine'
+
+const BookValidator = vine.compile(
+  vine.object({
+    title: vine.string().unique(async (db, value) => {
+      //only 1 ! because false if it exists and true if unique (book doesn't exists)
+      return !(await Book.query().where('title', value).first())
+    }),
+    numberOfPages: vine.number().positive(),
+    //null ok, undefined not ok
+    pdfLink: vine.string().nullable(),
+    abstract: vine.string().nullable(),
+    editionYear: vine.number().positive().max(2026).min(1600).nullable(),
+    imagePath: vine.string().nullable(),
+    //check if user/writer exist in db
+    userId: vine
+      .number()
+      .positive()
+      .exists(async (db, value) => {
+        return !!(await User.query().where('id', value).first())
+      }),
+    writerId: vine
+      .number()
+      .positive()
+      .exists(async (db, value) => {
+        return !!(await Writer.query().where('id', value).first())
+      }),
+  })
+)
+
+export { BookValidator }
