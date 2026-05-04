adf1cc61e976bf1d64124a278037ad489663b8a8
jonathan150408 <196329763+Jonathan150408@users.noreply.github.com>
2026-05-04 14:37:43 +0200

    feat(migration writers) : terminer la migration writers
    [5][Done]

---
+++ Backend/database/migrations/4_create_writers_table.ts
@@ -0,0 +1,24 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'writers'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      //default
+      table.increments('id')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+
+      //fields
+      table.string('firsname').notNullable()
+      table.string('lastname')
+
+      //no relation(s)
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
