c09897abb4acc1b679c2df1f778ed94464272759
jonathan150408 <196329763+Jonathan150408@users.noreply.github.com>
2026-05-04 13:13:11 +0200

    feat(factory book) : terminer la factory de book
    [15][Done]

---
+++ Backend/database/factories/book_factory.ts
@@ -0,0 +1,27 @@
+import factory from '@adonisjs/lucid/factories'
+import Book from '#models/book'
+import User from '#models/user'
+import Writer from '#models/writer'
+
+export const BookFactory = factory
+  .define(Book, async ({ faker }) => {
+    //select a random user/writer
+    const user = await User.query().orderByRaw('RAND').firstOrFail()
+    const writer = await Writer.query().orderByRaw('RAND').firstOrFail()
+
+    return {
+      title: faker.book.title(),
+      //arbitrary values for numberOfPages
+      numberOfPages: faker.number.int({ min: 100, max: 800 }),
+      pdfLink: faker.internet.url(),
+      //arbitrary values for the number of sentences in abstract
+      abstract: faker.lorem.paragraph({ min: 2, max: 16 }),
+      //semi-arbitrary values for the year
+      editionYear: faker.number.int({ min: 1600, max: 2026 }),
+      imagePath: faker.image.url(),
+      //uses the random selection
+      userId: user.id,
+      writerId: writer.id,
+    }
+  })
+  .build()
