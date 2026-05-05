b0ea2138786bbd2e60a0bff19e09e0831e7d1617
jonathan150408 <196329763+Jonathan150408@users.noreply.github.com>
2026-05-05 16:12:27 +0200

    fix(données catégorie et éditeur) : corriger le souci de unique
    [65][Done]
    Le problème était que le faker peut générer 2 fois la même donnée et que si le champ est UNIQUE, tout casse.

--- Backend/database/factories/categorie_factory.ts
+++ Backend/database/factories/categorie_factory.ts
@@ -1,10 +1,31 @@
 import factory from '@adonisjs/lucid/factories'
 import Categorie from '#models/categorie'
+//all the labels
+const labels: string[] = []
 
 export const CategorieFactory = factory
   .define(Categorie, async ({ faker }) => {
+    //pick a new genre if not unique
+    let label = ''
+    let isUnique
+
+    do {
+      isUnique = true
+      //pick a new random label
+      label = faker.book.genre()
+      //check if the label already exists
+      labels.forEach((lab) => {
+        if (lab === label) {
+          isUnique = false
+          return
+        }
+      })
+    } while (!isUnique)
+    labels.push(label)
+
+    //validate and create the new category
     return {
-      label: faker.book.genre(),
+      label: label,
     }
   })
   .build()
--- Backend/database/seeders/2_categorie_seeder.ts
+++ Backend/database/seeders/2_categorie_seeder.ts
@@ -3,7 +3,6 @@ import { BaseSeeder } from '@adonisjs/lucid/seeders'
 
 export default class extends BaseSeeder {
   async run() {
-    // Write your database queries inside the run method
-    await CategorieFactory.createMany(1)
+    await CategorieFactory.createMany(20)
   }
 }
