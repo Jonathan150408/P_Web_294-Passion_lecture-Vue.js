96a2cbd24a2a337f70ad520bf609c11246d413e8
jonathan150408 <196329763+Jonathan150408@users.noreply.github.com>
2026-05-05 17:20:59 +0200

    feat(model t_editor) : terminer le modèle t_editor
    [10][Done]

---
+++ Backend/app/models/t_editor.ts
@@ -0,0 +1,28 @@
+import Book from './book.js'
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Editor from './editor.js'
+
+export default class TEditor extends BaseModel {
+  //default
+  @column({ isPrimary: true })
+  declare id: number
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  //fields
+  
+  //relations
+  @column()
+  declare editorId : number
+  @belongsTo(() => Editor)
+  declare editor : BelongsTo<typeof Editor>
+
+  @column()
+  declare bookId : number
+  @belongsTo(() => Book)
+  declare book : BelongsTo<typeof Book>
+}
