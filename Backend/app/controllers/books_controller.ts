6e24d124e3919a0c0868d5703eb732ac5f542faa
jonathan150408 <196329763+Jonathan150408@users.noreply.github.com>
2026-05-05 15:06:37 +0200

    feat(controlleur book) : terminer le controlleur book
    [25][Done]

--- Backend/app/controllers/books_controller.ts
+++ Backend/app/controllers/books_controller.ts
@@ -1,4 +1,5 @@
 import Book from '#models/book'
+import { BookValidator } from '#validators/book'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class BooksController {
@@ -6,26 +7,86 @@ export default class BooksController {
    * Display a list of resource
    */
   async index({ response }: HttpContext) {
-    return await Book.query().preload('writer').orderBy('title', 'asc').exec()
+    response.ok(
+      await Book.query()
+        .preload('comments')
+        .preload('user')
+        .preload('writer')
+        .orderBy('title', 'asc')
+        .exec()
+    )
   }
 
   /**
    * Handle form submission for the create action
    */
-  async store({ request }: HttpContext) {}
+  async store({ request, response }: HttpContext) {
+    const { title, numberOfPages, pdfLink, abstract, editionYear, imagePath, userId, writerId } =
+      await request.validateUsing(BookValidator)
+    response.created(
+      await Book.create({
+        title,
+        numberOfPages,
+        pdfLink,
+        abstract,
+        editionYear,
+        imagePath,
+        userId,
+        writerId,
+      })
+    )
+  }
 
   /**
    * Show individual record
    */
-  async show({ params }: HttpContext) {}
+  async show({ params, response }: HttpContext) {
+    response.ok(
+      await Book.query()
+        .preload('comments')
+        .preload('user')
+        .preload('writer')
+        .where('id', params.id)
+        .firstOrFail()
+    )
+  }
 
   /**
    * Handle form submission for the edit action
    */
-  async update({ params, request }: HttpContext) {}
+  async update({ params, request, response }: HttpContext) {
+    const { title, numberOfPages, pdfLink, abstract, editionYear, imagePath, userId, writerId } =
+      await request.validateUsing(BookValidator)
+    const book = await Book.query()
+      .preload('comments')
+      .preload('user')
+      .preload('writer')
+      .where('id', params.id)
+      .firstOrFail()
+    book.merge({
+      title,
+      numberOfPages,
+      pdfLink,
+      abstract,
+      editionYear,
+      imagePath,
+      userId,
+      writerId,
+    })
+    response.ok(await book.save())
+  }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  async destroy({ params, response }: HttpContext) {
+    const book = await Book.query()
+      .preload('comments')
+      .preload('user')
+      .preload('writer')
+      .where('id', params.id)
+      .firstOrFail()
+    await book.delete()
+    response.noContent()
+  }
 }
