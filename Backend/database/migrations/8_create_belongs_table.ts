e30a58eeca3b2c92a64130482378dc2905b22f3e
jonathan150408 <196329763+Jonathan150408@users.noreply.github.com>
2026-05-11 13:57:23 +0200

    feat(migration belongs) : terminer la migration belongs
    [5][Done]

---
+++ Backend/database/migrations/8_create_belongs_table.ts
@@ -0,0 +1,27 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'belongs'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      //default
+      table.increments('id')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+
+      //fields
+
+      //relations
+      table.integer('category_id').notNullable().unsigned()
+      table.foreign('category_id').references('id').inTable('categories')
+      
+      table.integer('book_id').notNullable().unsigned()
+      table.foreign('book_id').references('id').inTable('books')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
