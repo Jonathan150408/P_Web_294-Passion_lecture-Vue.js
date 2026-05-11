6e5f49d96b84f6bc47f01522d4d6a2ce8fd06311
jonathan150408 <196329763+Jonathan150408@users.noreply.github.com>
2026-05-11 13:56:51 +0200

    feat(model belongs) : terminer le modèle belongs
    [10][Done]

---
+++ Backend/app/models/belong.ts
@@ -0,0 +1,27 @@
+import { DateTime } from 'luxon'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Categorie from './categorie.js'
+import Book from './book.js'
+
+export default class Belong extends BaseModel {
+  //default
+  @column({ isPrimary: true })
+  declare id: number
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  //fields
+  //relations
+  @column()
+  declare categoryId : Number
+  @belongsTo(() => Categorie)
+  declare category : BelongsTo<typeof Categorie>
+
+  @column()
+  declare bookId : Number
+  @belongsTo(() => Book)
+  declare book : BelongsTo<typeof Book>
+}
