e1ae6f136bc9382c2fda0a206b8194be562d1b20
jonathan150408 <196329763+Jonathan150408@users.noreply.github.com>
2026-05-11 14:13:54 +0200

    feat(seeder belongs) : terminer le seeder belongs
    [20][Done]

---
+++ Backend/database/seeders/8_belong_seeder.ts
@@ -0,0 +1,23 @@
+import Belong from '#models/belong'
+import Book from '#models/book'
+import Categorie from '#models/categorie'
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+
+export default class extends BaseSeeder {
+  async run() {
+      //get all data
+      const books = await Book.all()
+      const numberOfBooks = books.length
+      const categories = await Categorie.all()
+      const numberOfCategories = categories.length
+  
+      //each book receive a t_editor record
+      for (let i = 0; i < numberOfBooks; i++){
+        let bookId = books[i].id
+        //give from 0 to 3 categories
+        for (let i = 0; i < Math.round(Math.random() * 4); i++){
+          await Belong.create({ bookId, categoryId: categories[(Math.floor(Math.random() * numberOfCategories))].id })
+        }
+      }
+    }
+}
