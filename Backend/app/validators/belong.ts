abb91a466b6e9f69af082fa7a9cdf03d8856b4c7
jonathan150408 <196329763+Jonathan150408@users.noreply.github.com>
2026-05-12 16:33:18 +0200

    fix(validateur belong) : réparer le .exists()
    [30][Done]
    Je m'en suis rendu compte avec Bruno en testant le contrôleur de book (store).
    ça a pris du temps car l'erreur n'était pas explicite et se trouvait en partie dans le controlleur de book (fix -> je commit quand j'ai terminé)

--- Backend/app/validators/belong.ts
+++ Backend/app/validators/belong.ts
@@ -1,12 +1,13 @@
+import Categorie from '#models/categorie'
 import vine from '@vinejs/vine'
 
 const BelongValidator = vine.compile(
     vine.object({
         categorieId : vine.number().positive().exists(async( db, value ) => {
             //return true if found, false if not existing
-            return !!await db.query().where('id', value).first()
+            return !!await Categorie.query().where('id', value).first()
         })
-        //we don't need to validate the bookId because when created, we use the current route parametter (default 404 if not exists)
+        //we don't need to validate the bookId because when created, we use the new id
     })
 )
 
