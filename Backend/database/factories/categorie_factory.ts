529dbf313f5347bd7a2ca9b56f265331f2e419f5
neo-darbellay <178883466+neo-darbellay@users.noreply.github.com>
2026-05-18 12:58:19 +0200

    fix(Factories): Fait en sorte que les constraintes unique fonctionnent
    [28][Done]

--- Backend/database/factories/categorie_factory.ts
+++ Backend/database/factories/categorie_factory.ts
@@ -1,31 +1,20 @@
 import factory from '@adonisjs/lucid/factories'
 import Categorie from '#models/categorie'
-//all the labels
-const labels: string[] = []
+
+const usedLabels = new Set<string>()
 
 export const CategorieFactory = factory
   .define(Categorie, async ({ faker }) => {
-    //pick a new genre if not unique
-    let label = ''
-    let isUnique
+    let label: string
 
     do {
-      isUnique = true
-      //pick a new random label
       label = faker.book.genre()
-      //check if the label already exists
-      labels.forEach((lab) => {
-        if (lab === label) {
-          isUnique = false
-          return
-        }
-      })
-    } while (!isUnique)
-    labels.push(label)
+    } while (usedLabels.has(label))
 
-    //validate and create the new category
+    usedLabels.add(label)
+
     return {
-      label: label,
+      label,
     }
   })
   .build()
